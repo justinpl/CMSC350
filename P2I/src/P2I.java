821aa22e80dd1c397fa6b504380bb0a701a70639
Justin <jluce89@gmail.com>
2018-04-08 16:03:54 -0400

    update project 2

---
+++ P2I/src/P2I.java
@@ -0,0 +1,13 @@
+/**
+ * File Name   : P2I.java
+ * Author      : Justin Luce
+ * Created on  : 08-04-2018
+ * Description : 
+ **/
+public class P2I
+{
+    public static String convert() 
+    {
+        return "test";
+    }    
+}
